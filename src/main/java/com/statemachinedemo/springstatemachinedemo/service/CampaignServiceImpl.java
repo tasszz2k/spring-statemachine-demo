77169374e3c734343650200e06a38ab47e1ee534
Tass <tasszz2k@gmail.com>
2024-12-13 01:04:49 +0700

    update(code): remove unused code
    Signed-off-by: Tass <tasszz2k@gmail.com>

--- src/main/java/com/statemachinedemo/springstatemachinedemo/service/CampaignServiceImpl.java
+++ src/main/java/com/statemachinedemo/springstatemachinedemo/service/CampaignServiceImpl.java
@@ -40,115 +40,11 @@ public class CampaignServiceImpl implements CampaignService {
         .findById(id)
         .orElseThrow(() -> new RuntimeException("Campaign not found with id: " + id));
   }
-
-  @Deprecated
-  public Campaign transitionState(Long campaignId, CampaignEvent event, String changedBy) {
-    Campaign campaign = findById(campaignId);
-
-    // Initialize and configure the state machine with the current state
-    StateMachine<CampaignState, CampaignEvent> stateMachine =
-        stateMachineFactory.getStateMachine(campaignId.toString());
-    stateMachine
-        .getStateMachineAccessor()
-        .doWithAllRegions(
-            accessor ->
-                accessor.resetStateMachine(
-                    new DefaultStateMachineContext<>(
-                        campaign.getCurrentState(), null, null, null)));
-
-    // Start the state machine reactively
-    stateMachine.startReactively().block();
-
-    // Send the event reactively
-    stateMachine
-        .sendEvent(Mono.just(MessageBuilder.withPayload(event).build()))
-        .doOnNext(
-            result -> {
-              if (!result.getResultType().equals(StateMachineEventResult.ResultType.ACCEPTED)) {
-                List<CampaignEvent> acceptedEvents =
-                    stateMachine.getTransitions().stream()
-                        .filter(
-                            transition ->
-                                transition
-                                    .getSource()
-                                    .getId()
-                                    .equals(stateMachine.getState().getId()))
-                        .map(transition -> transition.getTrigger().getEvent())
-                        .toList();
-                // FIXME: this code is not working as expected
-                throw new IllegalStateException(
-                    "Invalid state transition. Accepted events: " + acceptedEvents);
-              }
-            })
-        .blockLast();
-
-    // Update campaign state and log the transition
-    CampaignState previousState = campaign.getCurrentState();
-    CampaignState nextState = stateMachine.getState().getId();
-
-    campaign.setCurrentState(nextState);
-    campaignRepository.save(campaign);
-
-    ActionLog log = new ActionLog();
-    log.setCampaign(campaign);
-    log.setPreviousState(previousState);
-    log.setNextState(nextState);
-    log.setEventTriggered(event);
-    log.setActor(changedBy);
-    actionLogRepository.save(log);
-
-    return campaign;
-  }
-
   @Override
   public Campaign transitionState(Long id, CampaignEvent event, CampaignParam param, String actor) {
     sendEvent(id, event, actor, param);
     // return the updated campaign
     return findById(id);
-  }
-
-  private StateMachine<CampaignState, CampaignEvent> build(Long campaignId) {
-    var campaign = findById(campaignId);
-    StateMachine<CampaignState, CampaignEvent> sm =
-        stateMachineFactory.getStateMachine(campaignId.toString());
-
-    // sm.stop() is deprecated, use sm.stopReactively() instead
-    sm.stop();
-
-    sm.getStateMachineAccessor()
-        .doWithAllRegions(
-            accessor -> {
-              accessor.addStateMachineInterceptor(campaignChangeInterceptor);
-              accessor.resetStateMachine(
-                  new DefaultStateMachineContext<>(campaign.getCurrentState(), null, null, null));
-            });
-    // sm.startReactively()
-    sm.start();
-    return sm;
-  }
-
-  private void sendEvent(Long campaignId, CampaignEvent event, String actor) {
-    sendEvent(campaignId, event, actor, null);
-  }
-
-  private void sendEvent(Long campaignId, CampaignEvent event, String actor, CampaignParam param) {
-    var sm = build(campaignId);
-
-    var msg =
-        MessageBuilder.withPayload(event)
-            .setHeader(CAMPAIGN_ID, campaignId)
-            .setHeader(ACTOR, actor)
-            .setHeader(CAMPAIGN_PARAM, param)
-            .build();
-
-    sm.sendEvent(Mono.just(msg))
-        .doOnNext(
-            result -> {
-              if (!result.getResultType().equals(StateMachineEventResult.ResultType.ACCEPTED)) {
-                throw new IllegalStateException("Invalid state transition");
-              }
-            })
-        .blockLast();
   }
 
   @Override
@@ -169,80 +65,44 @@ public class CampaignServiceImpl implements CampaignService {
     return savedCampaign;
   }
 
-  @Override
-  public StateMachine<CampaignState, CampaignEvent> submitForFAReview(Long id, String actor) {
-    sendEvent(id, CampaignEvent.SUBMIT, actor);
-    return null;
+  private StateMachine<CampaignState, CampaignEvent> build(Long campaignId) {
+    var campaign = findById(campaignId);
+    StateMachine<CampaignState, CampaignEvent> sm =
+            stateMachineFactory.getStateMachine(campaignId.toString());
+
+    // sm.stop() is deprecated, use sm.stopReactively() instead
+    sm.stop();
+
+    sm.getStateMachineAccessor()
+            .doWithAllRegions(
+                    accessor -> {
+                      accessor.addStateMachineInterceptor(campaignChangeInterceptor);
+                      accessor.resetStateMachine(
+                              new DefaultStateMachineContext<>(campaign.getCurrentState(), null, null, null));
+                    });
+    // sm.startReactively()
+    sm.start();
+    return sm;
   }
 
-  @Override
-  public StateMachine<CampaignState, CampaignEvent> reject(Long id, String actor) {
-    sendEvent(id, CampaignEvent.REJECT, actor);
-    return null;
+  private void sendEvent(Long campaignId, CampaignEvent event, String actor, CampaignParam param) {
+    var sm = build(campaignId);
+
+    var msg =
+            MessageBuilder.withPayload(event)
+                    .setHeader(CAMPAIGN_ID, campaignId)
+                    .setHeader(ACTOR, actor)
+                    .setHeader(CAMPAIGN_PARAM, param)
+                    .build();
+
+    sm.sendEvent(Mono.just(msg))
+            .doOnNext(
+                    result -> {
+                      if (!result.getResultType().equals(StateMachineEventResult.ResultType.ACCEPTED)) {
+                        throw new IllegalStateException("Invalid state transition");
+                      }
+                    })
+            .blockLast();
   }
 
-  @Override
-  public StateMachine<CampaignState, CampaignEvent> approve(Long id, String actor) {
-    sendEvent(id, CampaignEvent.APPROVE, actor);
-    return null;
-  }
-
-  @Override
-  public StateMachine<CampaignState, CampaignEvent> edit(
-      Long id, CampaignParam param, String actor) {
-    sendEvent(id, CampaignEvent.EDIT, actor);
-
-    return null;
-  }
-
-  @Override
-  public StateMachine<CampaignState, CampaignEvent> editAfterApproved(
-      Long id, CampaignParam param, String actor) {
-    sendEvent(id, CampaignEvent.EDIT, actor, param);
-
-    return null;
-  }
-
-  @Override
-  public StateMachine<CampaignState, CampaignEvent> terminate(Long id, String actor) {
-    sendEvent(id, CampaignEvent.TERMINATE, actor);
-
-    return null;
-  }
-
-  @Override
-  public StateMachine<CampaignState, CampaignEvent> distribute(Long id, String actor) {
-    sendEvent(id, CampaignEvent.DISTRIBUTE, actor);
-
-    return null;
-  }
-
-  @Override
-  public StateMachine<CampaignState, CampaignEvent> approveForUse(Long id, String actor) {
-    sendEvent(id, CampaignEvent.APPROVE, actor);
-
-    return null;
-  }
-
-  @Override
-  public StateMachine<CampaignState, CampaignEvent> editBudget(
-      Long id, CampaignParam param, String actor) {
-    sendEvent(id, CampaignEvent.EDIT, actor);
-
-    return null;
-  }
-
-  @Override
-  public StateMachine<CampaignState, CampaignEvent> rejectBudget(Long id, String actor) {
-    sendEvent(id, CampaignEvent.REJECT, actor);
-
-    return null;
-  }
-
-  @Override
-  public StateMachine<CampaignState, CampaignEvent> approveBudget(Long id, String actor) {
-    sendEvent(id, CampaignEvent.APPROVE, actor);
-
-    return null;
-  }
 }
